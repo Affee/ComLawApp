64b7a0874bf0cf59a7e246c8ef72bf185b7d2f09
affee <304442951@qq.com>
2018-10-26 18:43:23 +0800

    指数对数等

--- App.js
+++ App.js
@@ -7,38 +7,26 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,ListView} from 'react-native';
+import {Platform, StyleSheet,View,ListView,Image} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+//   android:
+//     'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
+// 导入json数据
 
 type Props = {};
 export default class App extends Component<Props> {
-
-  constructor(){
-    super();
-    const ds = new ListView.DataSource({rowHasChanged:(r1,r2) => r1 !== r2});
-    this.state = {
-      dataSource:ds.cloneWithProps(['row 1','row 2']),
-    };
-  }
   render() {
     return (
       <View style={{flex: 1}}>
-        <ListView dataSource={this.state.ds}
-                  renderRow={this.state.dataSource}
+      <h1>
+      ssds
+      </h1>
 
-
-        />
       </View>
-      // <ListView
-      //     dataSource={this.state.dataSource}
-      //     renderRow={(rowData) => <Text>{rowData}</Text>}
-      // />
     );
   }
 }
@@ -50,18 +38,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#42fff0',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    backgroundColor: '#ff5d56',
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: '#F5FCFF',
   },
 
 });
