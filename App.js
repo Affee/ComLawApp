db68d47ab77cfe6e34d183571295ce3a18e24ec7
affee <304442951@qq.com>
2018-11-02 16:49:15 +0800

    生活不易，且行且珍惜，哎，好好工作，好好生活吧

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet,View,ListView,Image} from 'react-native';
+import {StyleSheet,View,ListView,Image,TouchableOpacity,AlertIOS} from 'react-native';
 
 //json shuju
 const data = [
@@ -43,13 +43,16 @@ const data = [
     },
   ];
 
+var Dimensions = require('Dimensions');
+var {width} = Dimensions.get('window');
+
 
 export default class App extends Component<Props> {
   //构造函数数据的初始化
     constructor(props){
         super(props);
         this.state = {
-            var ds = new ListView.DataSource({rowHasChanged:(r1,r2)=>r1 !== r2});
+            let ds = new ListView.DataSource({rowHasChanged:(r1,r2)=>r1 !== r2});
             this.state = {
                 dataSource: ds.cloneWithRows(data)
             }
@@ -59,22 +62,71 @@ export default class App extends Component<Props> {
   render() {
     return (
       <ListView
+          style={ {marginTop: 25}}
           dataSource={this.state.dataSource}//设置数据源
-          renderRow={ this.render}
+          renderRow={ this.renderRow}
 
       />
     );
+  }
+  //返回具体的cell
+  renderRow(rowData,sectionID,rowID,highlightRow){
+    console.log(sectionID,rowID);
+    return(
+    <TouchableOpacity activeOpacity={0.5}
+                      onPress={()=>{AlertIOS.alert('购买成功','成功解锁'+rowData.name+'英雄',)}}
+    >
+        <View>
+            <Text style={styles.cellViewStyle}>
+                <Image source={require('../资源 1.png')} style={styles.leftImageStyle}/>
+                <View style={styles.rightViewStyle}>
+                    {/*英雄名称  英雄介绍*/}
+                    {/*<Text style = {styles.topTitleStyle}>*/}
+                        {/*{rowData.name}*/}
+                    {/*</Text>*/}
+                    {/*<Text style = {styles.bottomTitleStyle}*/}
+                          {/*numberOfLines={3}*/}
+                    {/*>{rowData.title}*/}
+                    {/*</Text>*/}
+
+                </View>
+
+            </Text>
+        </View>
+    </TouchableOpacity>
+    )
   }
 }
 
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
+    cellViewStyle:{
+    //    分割线
+        borderBottomWidth: 0.5,
+        borderBottomColor:'#e8e8e8',
+    //    cell内部缩下
+        padding :10,
+        flexDirection: 'row'
+
+
+    },
+    leftImageStyle:{
+        width: 60,
+        height: 60,
+        marginRight: 15,
+    },
+    rightViewStyle:{
+
+    },
+    topTitleStyle{
+        fontSize:20
+
+    },
+    bottomTitleStyle{
+        width:width*0.7,
+        marginTop: 5,
+
+    }
 
 });
---
+++ Car.js
@@ -0,0 +1,15 @@
+import React ,{Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    ListView,
+    Image,
+    TouchableOpacity,
+} from 'react-native';
+
+export  default  class Car extents Component{
+    render{
+        <View style={styles.container}
+    }
+}
